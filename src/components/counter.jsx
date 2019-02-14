f1b7e048860f3ef7fdefe439a81c64715f8efaa1
Surabhi Sharma <surabhisharma.226005@gmail.com>
2019-02-15 03:49:35 +0530

    event handling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,11 +11,21 @@ class Counter extends Component {
 
     return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
   }
+
+  handleIncrement() {
+    console.log("Increment clicked!");
+  }
+
   render() {
     return (
       <div>
-        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button> */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
 
         {this.state.tags.length === 0 && "Please create a new tag!"}
         {this.renderTags()}
@@ -24,16 +34,16 @@ class Counter extends Component {
     );
   }
 
-  // getBadgeClasses() {
-  //   let classes = "badge m-2 badge-";
-  //   classes += this.state.count === 0 ? "warning" : "primary";
-  //   return classes;
-  // }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-  // formatCount() {
-  //   const {count} = this.state;
-  //   return count === 0 ? "Zero" : count;
-  // }
+  formatCount() {
+    const {count} = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
  
 export default Counter;
