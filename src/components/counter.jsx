39ff90f70c1c0684a81a416e7f01a50910a16481
Surabhi Sharma <surabhisharma.226005@gmail.com>
2019-02-25 15:58:35 +0530

    passing event argument

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -19,9 +19,8 @@ class Counter extends Component {
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
-  handleIncrement = () => {
-    console.log("Increment clicked!", this);
-
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
     // event handlers don't have access to 'this'
   }
@@ -31,7 +30,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
