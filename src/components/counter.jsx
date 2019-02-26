591d882f36e96ba6a3e506124a48784594098d87
Surabhi Sharma <surabhisharma.226005@gmail.com>
2019-02-26 17:30:16 +0530

    destructuring arguements

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   render() {
+    const { onIncrement, counter, onDelete } = this.props;
+
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
+          onClick={() => onIncrement(counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
+          onClick={() => onDelete(counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
