d30fb0106564a266b8f49c9f9dfd2069c5c0f60a
Surabhi Sharma <surabhisharma.226005@gmail.com>
2019-02-26 16:30:27 +0530

    single source of truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,22 +11,40 @@ class Counters extends Component {
     ]
   }
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+
+    this.setState({ counters });
+  }
+
   handleDelete = counterId => {
     const counters = this.state.counters.filter(c => c.id !== counterId);
     this.setState({ counters });
   }
 
   render() {
-    return (<div>
-      {this.state.counters.map(counter => (
-        <Counter
-          key={counter.id}
-          onDelete={this.handleDelete}
-          counter={counter}
+    return (
+      <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
         >
-        </Counter>
-      ))}
-    </div>);
+          Reset
+        </button>
+
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          >
+          </Counter>
+        ))}
+      </div>
+    );
   }
 }
  
